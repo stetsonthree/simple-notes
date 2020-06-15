315d081dfdf8782b923b87768609b4a33e95f3c0
Lee Stetson <stetsonthree@icloud.com>
2020-06-15 14:15:06 -0400

    Work Session 3
    - Adds some Devise checks
    - Restricts access to user-owned notebooks
    - Prevents URL hacking
    - Enforces has_many relationship
    - Adds some minor styling

--- app/controllers/notebooks_controller.rb
+++ app/controllers/notebooks_controller.rb
@@ -1,10 +1,11 @@
 class NotebooksController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_notebook, only: [:show, :edit, :update, :destroy]
 
   # GET /notebooks
   # GET /notebooks.json
   def index
-    @notebooks = Notebook.all
+    @notebooks = current_user.notebooks
   end
 
   # GET /notebooks/1
@@ -24,7 +25,7 @@ class NotebooksController < ApplicationController
   # POST /notebooks
   # POST /notebooks.json
   def create
-    @notebook = Notebook.new(notebook_params)
+    @notebook = current_user.notebooks.build(notebook_params)
 
     respond_to do |format|
       if @notebook.save
@@ -64,11 +65,11 @@ class NotebooksController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_notebook
-      @notebook = Notebook.find(params[:id])
+      @notebook = current_user.notebooks.find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
     def notebook_params
-      params.require(:notebook).permit(:title, :user_id)
+      params.require(:notebook).permit(:title)
     end
 end
